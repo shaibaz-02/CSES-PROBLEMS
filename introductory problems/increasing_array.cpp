be6ed81149803434d881c87411c87d8076546955
shaibaz-02 <shaibazmd916@gmail.com>
2025-03-04 12:22:45 +0530

    update

---
+++ introductory problems/increasing_array.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	// your code goes here
+	int n;cin>>n;vector<int>arr(n);
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}int ans=0;
+	for(int i=1;i<n;i++){
+		if(arr[i]<arr[i-1]){
+			ans+=arr[i-1]-arr[i];
+			arr[i]=arr[i-1];
+		}
+	}
+	cout<<ans;
+	return 0;
+}
